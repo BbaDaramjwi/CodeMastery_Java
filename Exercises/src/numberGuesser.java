32a11b6d1aab352ad307e34ce8385743083b0ae7
BertShare <bertmshare@gmail.com>
2024-01-18 08:54:44 +0100

    update

---
+++ Exercises/src/numberGuesser.java
@@ -0,0 +1,31 @@
+/*
+    Exercis: 
+    Number Guesser
+    Mini-game where users input numbers (1-100), attempting to guess a randomly chosen number.
+    The game continues until the correct guess or entry of 0 to end the program.
+    Hints on whether the guess is too high or too low are provided after each input.
+*/
+
+
+import java.util.Random;
+
+public class numberGuesser
+{
+
+
+    public static void main (String[] args)
+    {
+
+       // int userInput = 0;
+    
+    }
+
+  //  public static ()
+  //  {
+  //  Random NumberGenerator = new Random();
+
+   // int NumberToGuess = NumberGenerator.nextInt(1,100);
+   // System.out.println(NumberToGuess);
+   // }
+
+}
